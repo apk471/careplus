4a0e47ff8e99dfa15249765ec478433900eed70e
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2024-08-06 22:08:41 +0530

    setup completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
+    <div>
+      <h1>Helo</h1>
+      <Button>Click me</Button>
+
+    </div>
     
   );
 }
